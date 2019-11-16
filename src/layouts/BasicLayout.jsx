2566edfa0c004448dfa689a6d429bac38f78800c
Yong Cheng <will.chengyong@gmail.com>
2019-11-16 20:00:50 +0800

    Changed footer.

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -37,26 +37,14 @@ const menuDataRender = menuList =>
 
 const defaultFooterDom = (
   <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
+    copyright="2019 SSPKU"
     links={[
       {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
-        blankTarget: true,
-      },
-      {
-        key: 'github',
-        title: <Icon type="github" />,
-        href: 'https://github.com/ant-design/ant-design-pro',
-        blankTarget: true,
-      },
-      {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
-        blankTarget: true,
-      },
+        key: 'Will, Cheng Yong',
+        title: 'Will, Cheng Yong',
+        href: '#',
+        blankTarget: false,
+      }
     ]}
   />
 );
